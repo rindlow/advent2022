ac3fe5985b93717d0f8d87a069a05eef47d3cd89
Henrik Rindlöw <henrik@rindlow.se>
2022-12-01 14:03:06 +0100

    Code cleanup

--- src/day1.rs
+++ src/day1.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::must_use_candidate)]
 #![allow(clippy::missing_panics_doc)]
-#![allow(clippy::stable_sort_primitive)]
+
+use itertools::Itertools;
 
 fn parse_file(filename: &str) -> Vec<Vec<i32>> {
     std::fs::read_to_string(filename)
@@ -20,19 +21,21 @@ fn parse_file(filename: &str) -> Vec<Vec<i32>> {
 pub fn most_calories_from_file(filename: &str) -> i32 {
     parse_file(filename)
         .iter()
-        .map(|elf| elf.iter().sum::<i32>())
+        .map(|v| v.iter().sum::<i32>())
         .max()
         .unwrap()
 }
 
 pub fn three_most_calories_from_file(filename: &str) -> i32 {
-    let mut cal = parse_file(filename)
+    parse_file(filename)
         .iter()
-        .map(|elf| elf.iter().sum::<i32>())
-        .collect::<Vec<i32>>();
-    cal.sort();
-    cal.reverse();
-    cal.get(0..3).unwrap().iter().sum()
+        .map(|v| v.iter().sum::<i32>())
+        .sorted_by(|a, b| Ord::cmp(b, a))
+        .collect::<Vec<i32>>()
+        .get(0..3)
+        .unwrap()
+        .iter()
+        .sum()
 }
 
 #[cfg(test)]
@@ -41,12 +44,12 @@ mod tests {
 
     #[test]
     fn part1() {
-        assert_eq!(24000, most_calories_from_file("testinput/day1.txt"));
-        assert_eq!(69912, most_calories_from_file("input/day1.txt"));
+        assert_eq!(24_000, most_calories_from_file("testinput/day1.txt"));
+        assert_eq!(69_912, most_calories_from_file("input/day1.txt"));
     }
     #[test]
     fn part2() {
-        assert_eq!(45000, three_most_calories_from_file("testinput/day1.txt"));
+        assert_eq!(45_000, three_most_calories_from_file("testinput/day1.txt"));
         assert_eq!(208_180, three_most_calories_from_file("input/day1.txt"));
     }
 }
