3965a5640f757b4ddf71451132362c48ad4baa5a
Henrik Rindlöw <henrik@rindlow.se>
2022-12-08 15:21:04 +0100

    Code cleanup

--- src/day8.rs
+++ src/day8.rs
@@ -1,148 +1,118 @@
+use itertools::Itertools;
 use std::fs::read_to_string;
 
-#[derive(Clone, Debug)]
-struct Grid {
-    size: usize,
-    rows: Vec<Vec<u32>>,
+type Grid = Vec<Vec<u32>>;
+
+fn parse_file(filename: &str) -> Grid {
+    read_to_string(filename)
+        .unwrap()
+        .lines()
+        .map(|line| line.chars().map(|d| d.to_digit(10).unwrap()).collect_vec())
+        .collect_vec()
 }
 
 fn is_hidden(x: usize, y: usize, grid: &Grid) -> bool {
-    if x == 0 || x == grid.size - 1 || y == 0 || y == grid.size - 1 {
+    let size = grid.len();
+    if x == 0 || x == size - 1 || y == 0 || y == size - 1 {
         return false;
     }
-    let height = grid.rows[y][x];
-    let mut viz = true;
-    for left in 0..x {
-        if grid.rows[y][left] >= height {
-            viz = false;
-            break;
-        }
-    }
-    if viz {
-        return false;
-    }
-    viz = true;
-    for up in 0..y {
-        if grid.rows[up][x] >= height {
-            viz = false;
-            break;
-        }
-    }
-    if viz {
-        return false;
-    }
-    viz = true;
-    for right in x + 1..grid.size {
-        if grid.rows[y][right] >= height {
-            viz = false;
-            break;
-        }
-    }
-    if viz {
-        return false;
-    }
-    viz = true;
-    for down in y + 1..grid.size {
-        if grid.rows[down][x] >= height {
-            viz = false;
-            break;
-        }
-    }
+    let height = grid[y][x];
 
-    !viz
+    // Ugly &&-chain to enable short circuit when first visible is found
+    grid[y].get(0..x).unwrap().iter().any(|h| *h >= height)
+        && grid[y]
+            .get(x + 1..size)
+            .unwrap()
+            .iter()
+            .any(|h| *h >= height)
+        && grid
+            .get(0..y)
+            .unwrap()
+            .iter()
+            .map(|row| row[x])
+            .any(|h| h >= height)
+        && grid
+            .get(y + 1..size)
+            .unwrap()
+            .iter()
+            .map(|row| row[x])
+            .any(|h| h >= height)
 }
 
-fn parse_file(filename: &str) -> Grid {
-    let mut grid = Grid {
-        size: 0,
-        rows: vec![],
-    };
-    for line in read_to_string(filename).unwrap().lines() {
-        let mut row: Vec<u32> = vec![];
-        if grid.size == 0 {
-            grid.size = line.len();
-        }
-        for tree in line.chars() {
-            let height = tree.to_digit(10).unwrap();
-            row.push(height);
-        }
-        grid.rows.push(row);
+fn score(x: usize, y: usize, grid: &Grid) -> u64 {
+    let size = grid.len();
+    if x == 0 || x == size - 1 || y == 0 || y == size - 1 {
+        return 0;
     }
-    grid
+    let height = grid[y][x];
+
+    let left = match grid[y]
+        .get(0..x)
+        .unwrap()
+        .iter()
+        .rev()
+        .enumerate()
+        .find(|(_, h)| **h >= height)
+    {
+        Some((i, _)) => i + 1,
+        None => x,
+    } as u64;
+
+    let right = match grid[y]
+        .get(x + 1..size)
+        .unwrap()
+        .iter()
+        .enumerate()
+        .find(|(_, h)| **h >= height)
+    {
+        Some((i, _)) => i + 1,
+        None => size - x - 1,
+    } as u64;
+
+    let up = match grid
+        .get(0..y)
+        .unwrap()
+        .iter()
+        .map(|row| row[x])
+        .rev()
+        .enumerate()
+        .find(|(_, h)| *h >= height)
+    {
+        Some((i, _)) => i + 1,
+        None => y,
+    } as u64;
+
+    let down = match grid
+        .get(y + 1..size)
+        .unwrap()
+        .iter()
+        .map(|row| row[x])
+        .enumerate()
+        .find(|(_, h)| *h >= height)
+    {
+        Some((i, _)) => i + 1,
+        None => size - y - 1,
+    } as u64;
+
+    left * right * up * down
 }
 
 pub fn visible(filename: &str) -> u64 {
     let grid = parse_file(filename);
-    let n = grid.size as u64 * grid.size as u64;
-    let viz: u64 = grid
-        .rows
-        .iter()
-        .enumerate()
-        .map(|(y, row)| {
-            row.iter()
-                .enumerate()
-                .filter(|(x, _)| is_hidden(*x, y, &grid))
-                .count() as u64
-        })
+    let size = grid.len();
+    let hidden: u64 = (0..size)
+        .map(|x| (0..size).filter(|y| is_hidden(x, *y, &grid)).count() as u64)
         .sum();
-    n - viz
-}
-
-fn score(x: usize, y: usize, grid: &Grid) -> u64 {
-    if x == 0 || x == grid.size - 1 || y == 0 || y == grid.size - 1 {
-        return 0;
-    }
-    let height = grid.rows[y][x];
-    let mut scr: u64 = 1;
-
-    let mut n = x as u64;
-    for xx in (0..x).rev() {
-        if grid.rows[y][xx] >= height {
-            n = x as u64 - xx as u64;
-            break;
-        }
-    }
-    scr *= n;
-
-    n = y as u64;
-    for yy in (0..y).rev() {
-        if grid.rows[yy][x] >= height {
-            n = y as u64 - yy as u64;
-            break;
-        }
-    }
-    scr *= n;
-
-    n = grid.size as u64 - 1 - x as u64;
-    for xx in x + 1..grid.size {
-        if grid.rows[y][xx] >= height {
-            n = xx as u64 - x as u64;
-            break;
-        }
-    }
-    scr *= n;
-
-    n = grid.size as u64 - 1 - y as u64;
-    for yy in y + 1..grid.size {
-        if grid.rows[yy][x] >= height {
-            n = yy as u64 - y as u64;
-            break;
-        }
-    }
-    scr *= n;
-
-    scr
+    (size * size) as u64 - hidden
 }
 
 pub fn highest_score(filename: &str) -> u64 {
     let grid = parse_file(filename);
-    let mut scores: Vec<u64> = vec![];
-    for y in 0..grid.size {
-        for x in 0..grid.size {
-            scores.push(score(x, y, &grid));
-        }
-    }
-    *scores.iter().max().unwrap()
+    let size = grid.len();
+    (0..size)
+        .map(|x| (0..size).map(|y| score(x, y, &grid)).max().unwrap())
+        .max()
+        .unwrap()
 }
 
 #[cfg(test)]
